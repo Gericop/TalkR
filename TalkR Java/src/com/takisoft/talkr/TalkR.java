48c4b8e7d0757c957683b6916d7bd4cb1abb955f
Gergely Kőrössy <korossyg@gmail.com>
2013-10-23 20:28:14 +0200

    initial commit

---
+++ TalkR Java/src/com/takisoft/talkr/TalkR.java
@@ -0,0 +1,15 @@
+package com.takisoft.talkr;
+
+/**
+ * The Hungarian chatter bot.
+ * @author Gericop
+ */
+public class TalkR {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TalkR talkr = new TalkR();
+    }
+}
