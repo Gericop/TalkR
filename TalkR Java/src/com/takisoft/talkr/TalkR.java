dc89048d75fdfff6049f8f2a03bb043a6b141cbe
Gergely Kőrössy <korossyg@gmail.com>
2013-10-23 21:37:36 +0200

    init commit 2

--- TalkR Java/src/com/takisoft/talkr/TalkR.java
+++ TalkR Java/src/com/takisoft/talkr/TalkR.java
@@ -1,15 +1,42 @@
 package com.takisoft.talkr;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 /**
  * The Hungarian chatter bot.
  * @author Gericop
  */
-public class TalkR {
-    
+public class TalkR extends JFrame {
+
+    boolean isWordDbReady = false;
+
+    public TalkR() {
+    }
+
+    public void initWindow() {
+        setTitle("TalkR v0.1");
+        setSize(640, 480);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void initData() {
+        if (!isWordDbReady) {
+            initDatabaseFromXML();
+        }
+    }
+
+    private void initDatabaseFromXML() {
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         TalkR talkr = new TalkR();
+        talkr.initWindow();
+        talkr.initData();
     }
 }
